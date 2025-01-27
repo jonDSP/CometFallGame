0d8b4b2aa5e87f203a0416bd34b66ff83d1d43be
Jonathan Desprez <jo.desprez@yahoo.com>
2025-01-27 15:33:35 +0100

    ajout image fond

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ SCREEN_HEIGHT = 720
 
 # generer la fenetre du jeu
 pygame.display.set_caption("Comet Fall Game")
-pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# charger l'arriere plan
+background = pygame.image.load("assets/bg.jpg")
 
 is_running = True
 
 # creation de la boucle du jeu
 while is_running:
+
+    # appliquer le background
+    screen.blit(background, (0,-200))
+
+    # mettre a jour l'ecran
+    pygame.display.flip()
 
     # detecter l'action de fermeture du jeu
     for event in pygame.event.get():  # renvoie une liste d'evenement, et on boucle dessus
