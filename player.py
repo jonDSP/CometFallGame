cf080bd0412bd39d1e750d81d0df82c6670cb62e
Jonathan Desprez <jo.desprez@yahoo.com>
2025-01-28 12:23:45 +0100

    ajout des projectiles du joueur

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+from projectile import Projectile
 
 # creer un classe representant le joueur
 class Player(pygame.sprite.Sprite):
@@ -7,11 +8,17 @@ class Player(pygame.sprite.Sprite):
         self.health = 100
         self.max_health = 100
         self.velocity = 2
+        self.all_projectiles = pygame.sprite.Group()
         self.attack = 5
         self.image = pygame.image.load("assets/player.png")
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 500
+
+    def launch_projectile(self):
+        # creer une nouvelle instance de Projectile
+        projectile = Projectile(self)
+        self.all_projectiles.add(projectile)
 
     def move_right(self):
         self.rect.x += self.velocity
